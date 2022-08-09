fb1a2fcf6d1ba234f8257d5931febe2553bb3a50
Rakesh Rath <rakeshrath.salu@gmail.com>
2022-08-09 11:33:00 +0530

    New files for demo

---
+++ DemoApp/ImageViewer/ImageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageViewer
+{
+    public class ImageInfo
+    {
+        public string Name { get; }
+        public string FullName { get; }
+
+        public ImageInfo(string name, string fullName)
+        {
+            Name = name;
+            FullName = fullName;
+        }
+    }
+}
---
+++ DemoApp/ImageViewer/ImagesRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageViewer
+{
+    public class ImagesRepo
+    {
+        public ObservableCollection<ImageInfo> Images { get; } = new();
+
+        public void GetImages(string folderPath)
+        {
+            Images?.Clear();
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            var files = di.GetFiles("*.jpg");
+
+            foreach(var file in files)
+            {
+                Images.Add(new ImageInfo(file.Name, file.FullName));
+            }
+        }
+    }
+}
