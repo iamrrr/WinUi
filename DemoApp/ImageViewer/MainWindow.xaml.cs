522bca143d79a9996680277ad87a23942a235885
Rakesh Rath <rakeshrath.salu@gmail.com>
2022-08-09 23:47:45 +0530

    Recent changes for demo

--- DemoApp/ImageViewer/MainWindow.xaml.cs
+++ DemoApp/ImageViewer/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,8 +18,39 @@ namespace ImageViewer
         public MainWindow()
         {
             this.InitializeComponent();
-            string folderPath = @"D:\Marriage\Bapi Bhai Pics";
-            ImagesRepo.GetImages(folderPath);
+            this.Title = "My Image VIewer";
+            //string folderPath = @"D:\Marriage\Bapi Bhai Pics";
+            //ImagesRepo.GetImages(folderPath);
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker browser = new FolderPicker();
+            browser.FileTypeFilter.Add("*");
+
+            var handle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WinRT.Interop.InitializeWithWindow.Initialize(browser, handle);
+
+            var folder = await browser.PickSingleFolderAsync();
+            if(folder != null)
+            {
+                ImagesRepo.GetImages(folder.Path);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var imageInfo = (sender as Button)?.DataContext as ImageInfo;
+            if(imageInfo != null)
+            {
+                var newWindow = new ImagePopup();
+                newWindow.Title = imageInfo.Name;
+                newWindow.SetPopupImage(new BitmapImage(new Uri(imageInfo.FullName, UriKind.Absolute)));
+                newWindow.Activate();
+            }
+            // C# code to create a new window
+            
+            
         }
     }
 }
